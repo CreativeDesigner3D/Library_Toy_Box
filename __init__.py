0cc38e775edc833d806e520fa95c0671dff33c45
Andrew Peel <34818078+CreativeDesigner3D@users.noreply.github.com>
2021-03-26 11:47:54 -0700

    Update Version Number

--- __init__.py
+++ __init__.py
@@ -11,8 +11,8 @@ from bpy.app.handlers import persistent
 bl_info = {
     "name": "Toy Box Library",
     "author": "Andrew Peel",
-    "version": (0, 2, 1),
-    "blender": (2, 91, 0),
+    "version": (0, 3, 0),
+    "blender": (2, 92, 0),
     "location": "Asset Library",
     "description": "This is a library for Blenders standard types and assemblies",
     "warning": "",
